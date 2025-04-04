bb65ee8e4d3fc8cee9ef123c5ca8e2d67c7c1e9e
MennaZaid <mennatallahzaid@aucegypt.edu>
2025-04-04 18:18:21 +0200

    required

--- cmake-build-debug/CTestTestfile.cmake
+++ cmake-build-debug/CTestTestfile.cmake
@@ -5,5 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test([=[example_test]=] "C:/Users/ASUS/Desktop/untitled10/cmake-build-debug/example.exe")
-set_tests_properties([=[example_test]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/ASUS/Desktop/untitled10/CMakeLists.txt;31;add_test;C:/Users/ASUS/Desktop/untitled10/CMakeLists.txt;0;")
+set_tests_properties([=[example_test]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/ASUS/Desktop/untitled10/CMakeLists.txt;28;add_test;C:/Users/ASUS/Desktop/untitled10/CMakeLists.txt;0;")
 subdirs("_deps/googletest-build")
